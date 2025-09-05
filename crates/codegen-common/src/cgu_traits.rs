da937545a51e5816f80ed656401eda19a0a97071
Graham Kelly <gkgoat6700@gmail.com>
2025-09-05 13:04:54 +0000

    a

--- crates/codegen-common/src/cgu_traits.rs
+++ crates/codegen-common/src/cgu_traits.rs
@@ -4,13 +4,26 @@ pub trait Core {
     type Type: Clone;
     type Var: Clone;
     fn decl_var(&mut self, t: Self::Type) -> Self::Var;
-    fn set(&mut self, target: Target<Self::Var>, val: Val<Self::Var>) -> Result<(), CoreError>;
+    fn set(
+        &mut self,
+        target: Target<Self::Var>,
+        val: Val<Self::Var>,
+        ty: Self::Type,
+    ) -> Result<(), CoreError>;
 
     type SSA: Clone;
-    fn get_var(&mut self, v: Val<Self::Var>) -> Option<Self::SSA> {
-        return None;
+    fn get_var(&mut self, v: Val<Self::Var>, ty: Self::Type) -> Result<Self::SSA, CoreError> {
+        return Err(CoreError::Unsupported);
     }
-    fn set_var(&mut self, val: Self::Var, s: Val<Self::SSA>) -> Result<(), CoreError> {
+    fn assemble_ssa(&mut self, v: Val<Self::SSA>, ty: Self::Type) -> Result<Self::SSA, CoreError> {
+        return Err(CoreError::Unsupported);
+    }
+    fn set_var(
+        &mut self,
+        val: Target<Self::Var>,
+        s: Val<Self::SSA>,
+        ty: Self::Type,
+    ) -> Result<(), CoreError> {
         return Err(CoreError::Unsupported);
     }
 
@@ -24,11 +37,13 @@ pub trait Core {
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 pub enum Target<T> {
     Just(T),
+    MemPtr { source: T },
 }
 #[non_exhaustive]
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 pub enum Val<T> {
     Just(T),
+    MemPtr { source: T },
 }
 #[non_exhaustive]
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
@@ -41,13 +56,11 @@ pub enum Term<T> {
 pub enum CoreError {
     Unsupported,
 }
-impl Display for CoreError{
+impl Display for CoreError {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        match self{
-            CoreError::Unsupported => write!(f,"unsupported"),
+        match self {
+            CoreError::Unsupported => write!(f, "unsupported"),
         }
     }
 }
-impl Error for CoreError{
-
-}
+impl Error for CoreError {}
